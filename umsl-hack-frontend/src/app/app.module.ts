2768f0cd8c1c4cd622fba2ed6b25f0d093230081
David Schwarb <dschwarb@architectnow.net>
2020-02-22 17:28:30 -0600

    added files

--- umsl-hack-frontend/src/app/app.module.ts
+++ umsl-hack-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 const appRoutes: Routes = [
   {
@@ -43,14 +44,15 @@ const appRoutes: Routes = [
     LotDisplayComponent,
     MapComponent,
     DashboardComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule,
     BrowserAnimationsModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
