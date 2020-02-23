a472b34a78bf840eba2ea5e8fcea2a9724137f05
Luke Dunakey <ldunakey@gmail.com>
2020-02-23 09:17:27 -0600

    Develop with Cheerio scraper working

---
+++ umsl-hack-frontend/src/app/Cheerio/index.js
@@ -0,0 +1,40 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+axios.get('https://myengage.umsl.edu/events.rss').then((response) => {
+const $ = cheerio.load(response.data.replace(/<!\[CDATA\[([\s\S]*?)\]\]>(?=\s*<)/gi, "$1"),{
+    xmlMode: true
+})
+const eventTitle = $('item div')
+for (let i = 0; i < 2; i++) {
+    const titleSpan = $(eventTitle[i]).find('.p-name')[0]
+    //console.log(eventTitle[i].children)
+    if (titleSpan){
+        const titleText = $(titleSpan).text()
+        console.log(titleText)
+    }
+}
+const eventLocation = $('item location')
+for (let i = 0; i < 1; i++) {
+    const titleSpan = $(eventLocation[i])
+    if(titleSpan){
+        const titleText = $(titleSpan).text()
+        console.log(titleText)
+    }
+}
+const eventStart = $('item start')
+for (let i = 0; i < 1; i++) {
+    const titleSpan = $(eventStart[i])
+    if(titleSpan){
+        const titleText = $(titleSpan).text()
+        console.log(titleText)
+    }
+}
+const eventEnd = $('item end')
+for (let i = 0; i < 1; i++) {
+    const titleSpan = $(eventEnd[i])
+    if(titleSpan){
+        const titleText = $(titleSpan).text()
+        console.log(titleText)
+    }
+}
+})
